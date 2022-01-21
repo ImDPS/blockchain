d0eb8901569b5fe81edaa76cb877996368e2dc50
Durgendra <dpsmad999@gmail.com>
2022-01-21 10:28:16 +0530

    add hexToBinary

--- src/crypto-hash.js
+++ src/crypto-hash.js
@@ -1,11 +1,14 @@
-const crypto = require("crypto");
+// const crypto = require("crypto");
+import crypto from "crypto";
+import hexToBinary from "hex-to-binary";
 
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash("sha256");
 
   hash.update(inputs.join(" "));
 
-  return hash.digest("hex");
+  // return hash.digest("hex");
+  return hexToBinary(hash.digest("hex"));
 };
 
-module.exports = cryptoHash;
+export default cryptoHash;
